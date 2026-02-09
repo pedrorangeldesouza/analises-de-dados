d59f70938caab914bdcf568389bc0d2fd58e2e83
Pedro Rangel <160076962+pedrorangeldesouza@users.noreply.github.com>
2026-02-09 20:18:14 -0300

    Update analise_vendas.sql

--- analise-vendas-rentabilidade/sql/analise_vendas.sql
+++ analise-vendas-rentabilidade/sql/analise_vendas.sql
@@ -1,1 +1,10 @@
-lmkmk
+SELECT
+    Categoria,
+    SUM(Quantidade * Preco) AS receita_total,
+    SUM(Quantidade * Custo) AS custo_total,
+    SUM(Quantidade * Preco) - SUM(Quantidade * Custo) AS lucro_total
+FROM vendas
+GROUP BY Categoria
+ORDER BY lucro_total DESC
+
+
